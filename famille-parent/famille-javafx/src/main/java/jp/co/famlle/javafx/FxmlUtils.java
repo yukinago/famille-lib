ee4bfc9ff94cac7de805bf12758590191410d037
famille <famille.soho@gmail.com>
2014-04-09 05:41:41 +0900

    新規作成

---
+++ famille-parent/famille-javafx/src/main/java/jp/co/famlle/javafx/FxmlUtils.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jp.co.famlle.javafx;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author famille
+ */
+public class FxmlUtils {
+        /**
+     * 新しい画面を生成する。
+     *
+     * @param owner 親画面
+     * @param root ルート要素
+     * @param title 画面タイトル
+     * @param isModal モーダルか否か
+     * @return 新規画面
+     */
+    public static Stage createStage(Stage owner, AnchorPane root, String title, boolean isModal) {
+        // 新しいウインドウを生成
+        Stage stage = new Stage();
+
+        // オーナーを設定
+        stage.initOwner(owner);
+
+        // モーダルフラグがONの場合はモーダルウインドウとする
+        if (isModal) {
+            stage.initModality(Modality.APPLICATION_MODAL);
+        }
+
+        // タイトルを設定する
+        stage.setTitle(title);
+
+        // シーンを設定する
+        stage.setScene(new Scene(root));
+
+        return stage;
+    }
+
+    /**
+     * FXMLファイルからルート要素を生成する。
+     *
+     * @param fxmlName FXMLファイル名
+     * @return ルート要素
+     */
+    public static AnchorPane loadRootFromFxml(String fxmlName) {
+        AnchorPane root = null;
+        try {
+            // fxmlファイルをロードする
+            root = FXMLLoader.load(FxmlUtils.class.getResource(fxmlName));
+        } catch (IOException ex) {
+            Logger.getLogger(FxmlUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return root;
+    }
+}
